c5eafb52b7d1c6a37135b4cd6d63d16dc1578199
Taro0504 <nyankoiscat0504@gmail.com>
2023-02-24 01:07:00 +0900

    create TopPage

---
+++ src/components/Results.js
@@ -0,0 +1,18 @@
+
+
+const Results = (props) => {
+  console.log("Resultsのprops", props)
+  return (
+    <div className="results-container">
+      <div>
+        <p>日付: {props.countryData.date.slice(0, 10)}</p>
+        <p>新規感染者: {props.countryData.newConfirmed.toLocaleString()}</p>
+        <p>感染者総数: {props.countryData.totalConfirmed.toLocaleString()}</p>
+        <p>新規回復者: {props.countryData.newRecovered.toLocaleString()}</p>
+        <p>回復車総数: {props.countryData.totalRecovered.toLocaleString()}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Results;
